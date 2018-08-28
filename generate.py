68630263ae21dd091dff500e3656efd15c3f8dce
Alex Norell <alex@alexnorell.com>
2018-08-28 02:25:21 -0700

    Added basic script for creating a pdf with pandoc

---
+++ generate.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+import os
+import argparse
+
+
+def argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "output")
+    parser.add_argument("-v", "verbosity", action="store_true")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = argument_parser()
+    os.system("pandoc README.md -o report.pdf")
+
+
+if __name__ == "__main__":
+    main()
