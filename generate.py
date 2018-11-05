010169ada9739d60f482a5e3eca48453b7cb6d18
Alex Norell <alex.norell@gmail.com>
2018-11-04 19:53:54 -0800

    Cleaned up generation script to follow PEP8

--- generate.py
+++ generate.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
+''' Report generation script for SJSU Senior Project '''
 
 import os
+import sys
 import argparse
 
 files = ["report.yaml",
@@ -11,27 +13,39 @@ files = ["report.yaml",
          "documents/chapter5_implementation.md",
          "documents/chapter6_tools.md",
          "documents/chapter7_testing.md",
-         #s"documents/chapter8_conclusion.md"
+         # "documents/chapter8_conclusion.md"
          ]
 
 
 def argument_parser():
+    ''' Argument parser for documentation generation script '''
     parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "output")
-    parser.add_argument("-v", "verbosity", action="store_true")
+    parser.add_argument("-o", "--output", default="report.pdf")
+    parser.add_argument("-v", "--verbosity", action="store_true")
     args = parser.parse_args()
     return args
 
 
-def main():
-    # args = argument_parser()
-    # Remove the report.pdf file before we build.
-    # Useful for artifacting
+def clean_pdf(to_remove):
+    ''' Function to delete previous pdf. Helps with Jenkins artifacting '''
     try:
-        os.remove("report.pdf")
+        os.remove(to_remove)
     except OSError:
         pass
-    os.system("pandoc " + " ".join(files) + " -s --template template.tex --pdf-engine=xelatex -N -o report.pdf")
+
+
+def main():
+    ''' Entry point into the report generation script '''
+    args = argument_parser()
+    clean_pdf(args.output)
+    template = "template.tex"
+    additional_pandoc_args = ["-s", "--template", template,
+                              "--pdf-engine=xelatex", "-N", "-o",
+                              args.output]
+    # print("pandoc " + " ".join(files + additional_pandoc_args))
+    os.system("pandoc " + " ".join(files + additional_pandoc_args))
+    if not os.path.isfile(args.output):
+        sys.exit(1)
 
 
 if __name__ == "__main__":
