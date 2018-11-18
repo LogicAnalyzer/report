a640efd0a544685329f2557b3df0ce5ba395d282
Alex Norell <alex.norell@gmail.com>
2018-11-18 03:32:59 -0800

    Got a dictionary of spelling mistakes

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,4 @@
 apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y apt-utils dirmngr gnupg gnupg-agent software-properties-common python3 autoconf gperf bison flex gcc g++ make swig python-dev cmake subversion iverilog python3-pip python-pip texlive-base texlive-fonts-extra texlive-fonts-recommended texlive-generic-recommended texlive-pictures texlive-xetex texlive-extra-utils xzdec
+sudo apt-get install -y apt-utils dirmngr gnupg gnupg-agent software-properties-common python3 autoconf gperf bison flex gcc g++ make swig python-dev cmake subversion iverilog python3-pip python-pip texlive-base texlive-fonts-extra texlive-fonts-recommended texlive-generic-recommended texlive-pictures texlive-xetex texlive-extra-utils xzdec aspell
 wget https://github.com/jgm/pandoc/releases/download/2.4/pandoc-2.4-1-amd64.deb && sudo dpkg -i pandoc-*-amd64.deb
 wget https://raw.githubusercontent.com/hotice/webupd8/master/install-google-fonts && sudo chmod +x install-google-fonts && sudo ./install-google-fonts
--- spell_check.py
+++ spell_check.py
@@ -1,7 +1,11 @@
+'''
+Spell Check script using aspell and python unit tests
+'''
 import os
 import sys
 import argparse
-
+import unittest
+import subprocess
 
 def argument_parser():
     ''' Argument parser for documentation generation script '''
@@ -22,15 +26,35 @@ def get_list_of_markdown_files():
                 diagrams.append(os.path.join(root, file).lstrip("./"))
     return diagrams
 
+
+def check_spelling_in_file(file, dictionary):
+    print_file = "cat ./" + file
+    check_file = "aspell -a --extra-dicts ./" + dictionary + " | grep -v '\*'"
+
+    p1 = subprocess.Popen(print_file, stdout=subprocess.PIPE, shell=True)
+    p2 = subprocess.Popen(check_file, stdin=p1.stdout, stdout=subprocess.PIPE, shell=True)
+    p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
+    output = p2.communicate()[0]
+    return_list = []
+    for line in output.decode("utf-8").split("\n"):
+        if line.startswith("&"):
+            return_list.append(line.split()[1:4])
+    if return_list.__len__():
+        return return_list
+
+
 def main():
     ''' Main function of spell_check.py'''
     args = argument_parser()
     files = get_list_of_markdown_files()
+    all_mistakes = {}
     for file in files:
-        print("\n\n" + file + ":")
-        os.system("cat " + file + " | aspell -a --extra-dicts ./" +
-            args.dictionary + " | grep '&'")
-    return 0
+        spelling_mistakes = check_spelling_in_file(file, args.dictionary)
+        if spelling_mistakes is not None:
+            all_mistakes[file] = spelling_mistakes
+    print(all_mistakes)
 
 if __name__ == "__main__":
     main()
+    #import xmlrunner
+    #unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
