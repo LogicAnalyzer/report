7470ab5b997bb3ba9cf1656c2ed87df53368c73b
Alex Norell <alex.norell@gmail.com>
2018-11-18 15:20:36 -0800

    Added a dependency

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,5 @@
 apt-get update
-sudo apt-get install -y apt-utils dirmngr gnupg gnupg-agent software-properties-common python3 autoconf gperf bison flex gcc g++ make swig python-dev cmake subversion iverilog python3-pip python-pip texlive-base texlive-fonts-extra texlive-fonts-recommended texlive-generic-recommended texlive-pictures texlive-xetex texlive-extra-utils xzdec aspell
+sudo apt-get install -y apt-utils dirmngr gnupg gnupg-agent software-properties-common python3 autoconf gperf bison flex gcc g++ make swig python-dev cmake subversion iverilog python3-pip python-pip texlive-base texlive-fonts-extra texlive-fonts-recommended texlive-generic-recommended texlive-pictures texlive-xetex texlive-extra-utils xzdec aspell texlive-latex-recommended
 
 if ! [ -x "$(command -v pandoc)" ]; then 
     # Fonts
